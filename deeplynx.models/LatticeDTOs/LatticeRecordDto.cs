e233e21a1c59ec693628a6511b1c8fb4a70ce07a
Jaren Brownlee <jaren.brownlee@inl.gov>
2026-01-20 09:59:47 -0600

    fix datatypes

--- deeplynx.models/LatticeDTOs/LatticeRecordDto.cs
+++ deeplynx.models/LatticeDTOs/LatticeRecordDto.cs
@@ -5,7 +5,7 @@ namespace deeplynx.models;
 public class LatticeRecordDto
 {
     [Column("record_name")] public string RecordName { get; set; } = null!;
-    [Column("record_description")] public string RecordDescription { get; set; } = null!;
-    [Column("record_properties")] public string RecordProperties { get; set; } = null!;
-    [Column("class_name")] public string ClassName { get; set; } = null!;
+    [Column("record_description")] public string? RecordDescription { get; set; } = null!;
+    [Column("record_properties")] public string? RecordProperties { get; set; } = null!;
+    [Column("class_name")] public string? ClassName { get; set; } = null!;
 }
--- deeplynx.models/LatticeDTOs/LatticeRelationshipDto.cs
+++ deeplynx.models/LatticeDTOs/LatticeRelationshipDto.cs
@@ -4,9 +4,9 @@ namespace deeplynx.models;
 
 public class LatticeRelationshipDto
 {
-    [Column("origin_class_name")] public string OriginClassName { get; set; } = null!;
-    [Column("relationship_name")] public string ClassName { get; set; } = null!;
-    [Column("relationship_description")] public string ClassDescription { get; set; } = null!;
-    [Column("relationship_properties")] public string ClassProperties { get; set; } = null!;
-    [Column("destination_class_name")] public string DestinationClassName { get; set; } = null!;
+    [Column("origin_class_name")] public string? OriginClassName { get; set; } = null!;
+    [Column("relationship_name")] public string RelationshipName { get; set; } = null!;
+    [Column("relationship_description")] public string? RelationshipDescription { get; set; } = null!;
+    [Column("relationship_properties")] public string? RelationshipProperties { get; set; } = null!;
+    [Column("destination_class_name")] public string? DestinationClassName { get; set; } = null!;
 }
