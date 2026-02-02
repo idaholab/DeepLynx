eafb5327c7047a1d54ebc87e9b387996ee68b8c6
Natalie Hergesheimer <natalie.hergesheimer@inl.gov>
2026-02-02 09:56:10 -0700

    IM SORRY OK WHAT DO YOU WANT FROM ME

--- deeplynx.UI/src/proxy.ts
+++ deeplynx.UI/src/proxy.ts
@@ -3,8 +3,6 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { auth } from "../auth";
 
-export const runtime = "nodejs";
-
 export async function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
   const isAuthDisabled =
