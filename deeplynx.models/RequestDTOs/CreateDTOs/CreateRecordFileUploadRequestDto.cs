6c12ca7d1c64d8fd557744f3f27b83d258768df1
Natalie Hergesheimer <natalie.hergesheimer@inl.gov>
2026-02-24 14:48:32 -0700

    dto update

--- deeplynx.models/RequestDTOs/CreateDTOs/CreateRecordFileUploadRequestDto.cs
+++ deeplynx.models/RequestDTOs/CreateDTOs/CreateRecordFileUploadRequestDto.cs
@@ -1,24 +1,19 @@
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Nodes;
-using System.Text.Json.Serialization;
 
 namespace deeplynx.models;
 
 public class CreateRecordFileUploadRequestDto
 {
-    [Required]
-    public string Name { get; set; }
-    
-    [Required]
-    public string Description { get; set; }
-    
-    [Required]
-    public JsonObject Properties { get; set; }
-    
-    [Required]
-    public string OriginalId { get; set; }
-    
+    [Required] public string Name { get; set; }
+
+    [Required] public string Description { get; set; }
+
+    [Required] public JsonObject Properties { get; set; }
+
+    [Required] public string OriginalId { get; set; }
+
     public long? ClassId { get; set; }
-    
-    public string? ClassName { get; set; } 
+
+    public string? ClassName { get; set; }
 }
