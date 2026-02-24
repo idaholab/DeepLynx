b7012f73d855600f128d31b0ab5d80400d5dc707
Natalie Hergesheimer <natalie.hergesheimer@inl.gov>
2026-02-24 08:53:23 -0700

    more file name sani

--- deeplynx.business/TimeseriesBusiness.cs
+++ deeplynx.business/TimeseriesBusiness.cs
@@ -385,7 +385,6 @@ public class TimeseriesBusiness(
         string tableName)
     {
         file = new SanitizedFormFile(file);
-        tableName = SanitizedFormFile.SanitizeFileName(tableName);
         var fileType = Path.GetExtension(file.FileName);
         if (fileType != ".csv" && fileType != ".parquet")
             throw new ArgumentException("Only CSV and Parquet files are supported.");
