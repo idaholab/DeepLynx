c4104740ac3919af18dfe1376c1f299cf393cd2a
Natalie Hergesheimer <natalie.hergesheimer@inl.gov>
2026-04-08 12:32:09 -0600

    pgvector package

--- deeplynx.api/Program.cs
+++ deeplynx.api/Program.cs
@@ -12,6 +12,8 @@ using Microsoft.AspNetCore.Http.Features;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi;
+using Npgsql;
+using Pgvector.Npgsql;
 using Scalar.AspNetCore;
 using Serilog;
 using Log = Serilog.Log;
@@ -132,8 +134,12 @@ try
     */
     builder.Services.AddHttpContextAccessor();
 
+    var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
+    dataSourceBuilder.UseVector();
+    var dataSource = dataSourceBuilder.Build();
+
     builder.Services.AddDbContext<DeeplynxContext>(
-        options => options.UseNpgsql(connectionString),
+        options => options.UseNpgsql(dataSource),
         ServiceLifetime.Transient
     );
 
