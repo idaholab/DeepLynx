ee51fe70e5cad93fba6e46241817f2e5c6fbe983
Jaren Brownlee <jaren.brownlee@inl.gov>
2026-04-13 15:53:43 -0500

    staging migration

--- deeplynx.datalayer/Migrations/Staging/20260413204828_AddFileSizeColumn.Designer.cs
+++ deeplynx.datalayer/Migrations/Staging/20260413204828_AddFileSizeColumn.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using deeplynx.datalayer.Models;
@@ -11,9 +12,11 @@ using deeplynx.datalayer.Models;
 namespace deeplynx.datalayer.Migrations.Staging
 {
     [DbContext(typeof(StagingContext))]
-    partial class StagingContextModelSnapshot : ModelSnapshot
+    [Migration("20260413204828_AddFileSizeColumn")]
+    partial class AddFileSizeColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
