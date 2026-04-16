7afcf830958c77097f94ea1e1d4fdef1cd92cafc
Jaren Brownlee <jaren.brownlee@inl.gov>
2026-04-16 12:45:03 -0500

    fix staging problems

--- deeplynx.datalayer/Models/StagingContext.cs
+++ deeplynx.datalayer/Models/StagingContext.cs
@@ -45,6 +45,7 @@ public class StagingContext : DbContext
         modelBuilder.Ignore<Permission>();
         modelBuilder.Ignore<Project>();
         modelBuilder.Ignore<ProjectMember>();
+        modelBuilder.Ignore<RecordCollection>();
         modelBuilder.Ignore<Role>();
         modelBuilder.Ignore<SavedSearch>();
         modelBuilder.Ignore<SensitivityLabel>();
@@ -72,6 +73,12 @@ public class StagingContext : DbContext
             .ToTable("GroupUser", "staging", t => t.ExcludeFromMigrations());
         modelBuilder.Entity("PermissionRole")
             .ToTable("PermissionRole", "staging", t => t.ExcludeFromMigrations());
+        modelBuilder.Entity("RecordCollectionSensitivityLabel")
+            .ToTable("RecordCollectionSensitivityLabel", "staging", t => t.ExcludeFromMigrations());
+        modelBuilder.Entity("RecordCollectionTag")
+            .ToTable("RecordCollectionTag", "staging", t => t.ExcludeFromMigrations());
+        modelBuilder.Entity("RecordRecordCollection")
+            .ToTable("RecordRecordCollection", "staging", t => t.ExcludeFromMigrations());
 
         modelBuilder.Entity<Class>(entity =>
         {
