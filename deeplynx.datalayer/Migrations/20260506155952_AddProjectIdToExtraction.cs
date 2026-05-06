bb3acf58a38c569b972be490d8ef84be68598123
Natalie Hergesheimer <natalie.hergesheimer@inl.gov>
2026-05-06 10:08:41 -0600

    yeah I know I know, I forgor

---
+++ deeplynx.datalayer/Migrations/20260506155952_AddProjectIdToExtraction.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace deeplynx.datalayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProjectIdToExtraction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "project_id",
+                schema: "deeplynx",
+                table: "extractions",
+                type: "bigint",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "project_id",
+                schema: "deeplynx",
+                table: "extractions");
+        }
+    }
+}
