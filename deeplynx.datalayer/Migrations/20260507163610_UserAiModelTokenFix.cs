8b329354118ecacc37ba890bb7faf4d9d17c7fa9
Keaton A. Flake <keaton.flake@inl.gov>
2026-05-07 13:34:59 -0600

    sweet repentence

---
+++ deeplynx.datalayer/Migrations/20260507163610_UserAiModelTokenFix.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace deeplynx.datalayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserAiModelTokenFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM deeplynx.user_model_tokens");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
