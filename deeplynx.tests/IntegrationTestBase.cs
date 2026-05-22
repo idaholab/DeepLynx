ad64c961689a37e0ee29f179c7a11bd3cacdd530
Edward Ramos <Edward.Ramos@inl.gov>
2026-05-22 15:10:05 -0600

    Update IntegrationTestBase.cs

--- deeplynx.tests/IntegrationTestBase.cs
+++ deeplynx.tests/IntegrationTestBase.cs
@@ -90,7 +90,7 @@ public class TestSuiteFixture : IAsyncLifetime
 
 // Defines a test collection named "Test Suite Collection".
 // This collection uses the TestSuiteFixture class for setup and teardown.
-[CollectionDefinition("Test Suite Collection")]
+[CollectionDefinition("Test Suite Collection", DisableParallelization = true)]
 public class TestSuiteCollection : ICollectionFixture<TestSuiteFixture>
 {
     // This class has no code, and is never created. Its purpose is simply
