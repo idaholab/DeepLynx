a69e29b49444ccff2ba15e91123287c4ceba663b
Sarah Ann Dombroski <dombsa@inl654159.inel.gov>
2026-06-02 15:51:07 -0600

    modified functions that were using the API calls that were changed previously

--- deeplynx.UI/src/app/(home)/types/types.ts
+++ deeplynx.UI/src/app/(home)/types/types.ts
@@ -2,9 +2,9 @@
 
 import React, { ReactNode } from 'react';
 import { CustomQueryRequestDto } from './requestDTOs';
-import { HistoricalRecordResponseDto, RecordResponseDto } from './responseDTOs';
+import { QueryRecordViewResponseDto } from './responseDTOs';
 
-export type RecordTableRow = HistoricalRecordResponseDto & {
+export type RecordTableRow = QueryRecordViewResponseDto & {
   fileType: string;
   timeseries?: boolean;
   fileSize?: number;
