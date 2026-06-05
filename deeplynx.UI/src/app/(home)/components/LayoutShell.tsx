e768ee7b061b7f17ef94e697a3e7f6d5d4cf7800
Sarah Ann Dombroski <dombsa@inl654159.inel.gov>
2026-06-05 13:50:49 -0600

    added horizontal scrolling when content overflows

--- deeplynx.UI/src/app/(home)/components/LayoutShell.tsx
+++ deeplynx.UI/src/app/(home)/components/LayoutShell.tsx
@@ -275,7 +275,7 @@ const LayoutShell = ({ children }: { children: ReactNode }) => {
         </div>
       </header>
       {/* Page Content */}
-      <div className="flex h-full z-0 mt-6">
+      <div className="flex h-full z-0 mt-6 w-full overflow-x-auto">
         {isMobileNavOpen && (
           <button
             type="button"
@@ -438,9 +438,9 @@ const LayoutShell = ({ children }: { children: ReactNode }) => {
           onMobileClose={() => setIsMobileNavOpen(false)}
         />
         <main
-          className={`transition-all duration-300 w-full mt-20 ml-0 ${
-            isMenuCollapsed ? "lg:ml-40" : "lg:ml-82"
-          }`}
+          className={`transition-all duration-300 min-w-[750px] flex-1 w-full mt-20 ml-0 ${
+              isMenuCollapsed ? "lg:ml-40" : "lg:ml-82"
+            }`}
         >
           {/* Organization Banner */}
           <div className="sticky top-25 z-20">
