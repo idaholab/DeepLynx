b86b44f015e8c6bf52edee1094738dd10fa41454
Jaren Brownlee <jaren.brownlee@inl.gov>
2026-06-22 13:55:56 -0500

    add artifact hashes migration

--- deeplynx.datalayer/Migrations/20260622185526_AddArtifactHashes.Designer.cs
+++ deeplynx.datalayer/Migrations/20260622185526_AddArtifactHashes.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using deeplynx.datalayer.Models;
@@ -11,9 +12,11 @@ using deeplynx.datalayer.Models;
 namespace deeplynx.datalayer.Migrations
 {
     [DbContext(typeof(DeeplynxContext))]
-    partial class DeeplynxContextModelSnapshot : ModelSnapshot
+    [Migration("20260622185526_AddArtifactHashes")]
+    partial class AddArtifactHashes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
