032ac2f7abdf4e3fcef8eadc18ce1925884cd141
Michael Crapse <michael.crapse@inl.gov>
2026-06-22 12:21:49 -0600

    Add embedding request dto

---
+++ deeplynx.models/RequestDTOs/EmbeddedRequestDto.cs
@@ -0,0 +1,14 @@
+namespace deeplynx.models;
+
+/// <summary>
+/// Specifies embedded status of a record
+/// </summary>
+public enum EmbeddedRequestDto
+{
+    /// <summary>
+    /// The record is either embedded or not embedded
+    /// </summary>
+    Any,
+    Embedded,
+    NotEmbedded,
+}
