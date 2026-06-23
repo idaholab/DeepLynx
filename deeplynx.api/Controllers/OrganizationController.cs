06702895168a8f8c30b7e014d407d5e0c8a29883
Jason Kuipers <jason.kuipers@inl.gov>
2026-06-23 09:01:09 -0600

    removed uneeded auth annotations

--- deeplynx.api/Controllers/OrganizationController.cs
+++ deeplynx.api/Controllers/OrganizationController.cs
@@ -305,9 +305,7 @@ public class OrganizationController : ControllerBase
     /// <param name="userName"></param>
     /// <returns></returns>
     [HttpPost("{organizationId:long}/invite", Name = "api_invite_user_to_organization")]
-    [SysAdmin] // skip permission checks for sys admins
-    [OrgAdmin] // skip permission checks for org admins
-    [ProjectAdmin] //skip permission checks for project admins
+    [ProjectAdmin] 
     public async Task<ActionResult> InviteUserToOrganization(
         long organizationId,
         [FromQuery] string userEmail,
