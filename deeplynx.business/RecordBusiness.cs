e4e7c3a1b60ca8f024876ba413fdb815dad8ea25
Curtis Michael Hill <hillcm3@inl621836.inel.gov>
2026-07-07 10:27:08 -0600

    Some merge changes

--- deeplynx.business/RecordBusiness.cs
+++ deeplynx.business/RecordBusiness.cs
@@ -32,8 +32,6 @@ public class RecordBusiness : IRecordBusiness
     /// <param name="tagBusiness">Used for creating tags related to a record.</param>
     /// <param name="labelBusiness">Used for creating tags related to a record.</param>
     /// <param name="sensitivityLabelService">Service for sensitivity label authorization operations.</param>
-    /// <param name="objectStorageBusiness">Used for object storage management.</param>
-    /// <param name="fileBusinessFactory">Used for file management.</param>
     public RecordBusiness(
         DeeplynxContext context,
         IEventBusiness eventBusiness,
@@ -935,7 +933,7 @@ public class RecordBusiness : IRecordBusiness
         if (dto.Properties == null)
             throw new ArgumentNullException(nameof(dto.Properties), "Properties cannot be null");
 
-        var maxDepth = ValidationHelper.ValidateJsonMaxDepth(dto.Properties);
+        var maxDepth = CalculateJsonMaxDepth(dto.Properties);
         if (maxDepth > 3)
             throw new Exception(
                 $"The depth of the JSON structure exceeds the maximum allowed depth of 3. Current depth of properties is {maxDepth}.");
@@ -1659,7 +1657,7 @@ public class RecordBusiness : IRecordBusiness
         if (returnedRecord is null)
             throw new KeyNotFoundException($"Record with id {recordId} not found");
 
-        var maxDepth = ValidationHelper.ValidateJsonMaxDepth(dto.Properties);
+        var maxDepth = CalculateJsonMaxDepth(dto.Properties);
         if (maxDepth > 3)
             throw new Exception(
                 $"The depth of the JSON structure exceeds the maximum allowed depth of 3. Current depth of properties is {maxDepth}.");
@@ -1892,6 +1890,35 @@ public class RecordBusiness : IRecordBusiness
                 Name = l.Name
             }).ToList()
         }).ToList();
+    }
+
+    /// <summary>
+    ///     Private method used to calculate json depth of properties (should be less than three)
+    /// </summary>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    private int CalculateJsonMaxDepth(JsonNode? node)
+    {
+        if (node is not JsonObject && node is not JsonArray)
+            return 0;
+
+        var maxDepth = 0;
+        if (node is JsonObject jsonObject)
+            foreach (var prop in jsonObject)
+            {
+                var depth = CalculateJsonMaxDepth(prop.Value);
+                if (depth > maxDepth)
+                    maxDepth = depth;
+            }
+        else if (node is JsonArray jsonArray)
+            foreach (var item in jsonArray)
+            {
+                var depth = CalculateJsonMaxDepth(item);
+                if (depth > maxDepth)
+                    maxDepth = depth;
+            }
+
+        return maxDepth + 1;
     }
 
     /// <summary>
