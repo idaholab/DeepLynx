6fb16ed4190b2fef60ee9f4da901bc8b04451703
Keaton A. Flake <keaton.flake@inl.gov>
2026-08-05 13:28:42 -0600

    project admin from middleware

--- deeplynx.api/Controllers/RecordController.cs
+++ deeplynx.api/Controllers/RecordController.cs
@@ -856,7 +856,7 @@ public class RecordController : ControllerBase
     {
         try
         {
-            bool isAdmin = UserContextStorage.IsSysAdmin || UserContextStorage.IsOrgAdmin;
+            bool isAdmin = UserContextStorage.IsSysAdmin || UserContextStorage.IsOrgAdmin || UserContextStorage.IsProjectAdmin;
             var edges = await _graphBusiness.GetGraphDataForRecord(
                 organizationId, projectId, recordId, UserContextStorage.UserId, depth, isAdmin);
             return Ok(edges);
