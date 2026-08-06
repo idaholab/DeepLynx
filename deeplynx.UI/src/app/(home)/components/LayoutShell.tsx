67c08a3f14513a061eb8a05dfd01204476542811
Jaren Brownlee <jaren.brownlee@inl.gov>
2026-08-06 16:51:32 -0500

    version bump

--- deeplynx.UI/src/app/(home)/components/LayoutShell.tsx
+++ deeplynx.UI/src/app/(home)/components/LayoutShell.tsx
@@ -445,7 +445,7 @@ const LayoutShell = ({ children }: { children: ReactNode }) => {
                   <QuestionMarkCircleIcon className="size-10" />
                 </Link>
               </li>
-              <span className="text-xs font-bold text-base-200/50">v0.6.1</span>
+              <span className="text-xs font-bold text-base-200/50">v0.7.2</span>
             </ul>
           </aside>
         </div>
